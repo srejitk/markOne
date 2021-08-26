4ed32346d014a725fb418500ecfb139936b6e971
Sreejith K <kumar.edwardcullen.sreejith80@gmail.com>
2021-08-26 17:59:53 +0000

    fix : Corrected questions and readme link

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# CLI Quiz App
-## Mark 1 Assessment : How well do you know me?
+# MCU CLI Quiz App
+## Mark 2 Assessment : How well do you know me?
 
-This is a command line interface quiz which'll ask you your username, ask questions related to you'rs truly and provide your score at the end.
+This is a command line interface quiz which'll ask you your username, ask questions related to the marvel cinematic universe and provide your score at the end.
 Have fun!
 
-https://replit.com/@SreejithKumar/Neogcamp-mark-one?v=1
+https://replit.com/@SreejithKumar/Neogcamp-mark-two?v=1
 
 P.S. Hit me up @srejitk if you end up scoring the highest 😉
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 var scoreboard = 0
 
 function welcomeMessage() {
-  var userName = readlineSync.question(chalk.bold.red("Hi!, What's your name ") + chalk.bold.white.bgRed("buddy?\n"));
-  console.log(chalk.bold.blue("Welcome to How well do you know Sreejith,") + chalk.bold.white.bgBlue(userName) + "!!\n");
+  var userName = readlineSync.question(chalk.bold.red("Hi!, What's your name ") + chalk.bold.white.bgRed("Challenger\n"));
+  console.log(chalk.bold.blue("Welcome to How well do you know the MCU,Challenger ") + chalk.bold.white.bgBlue(userName) + "!!\n");
 
   console.log(chalk.bold.yellow("Let's see if you really know me after all\n"));
 }
@@ -15,7 +15,7 @@ function play(question, answer) {
     console.log(chalk.bold.green("Not Bad Kid. Not Bad at all!\n"))
     scoreboard = scoreboard + 1;
   } else {
-    console.log(chalk.red.bold("*Evil Laugh*! And you thought you knew me well?!?\n"))
+    console.log(chalk.red.bold("*Evil Laugh*! You should have gone for the head\n"))
     scoreboard = scoreboard - 1;
   }
   console.log("Current Score :" + scoreboard + "\n");
@@ -30,22 +30,22 @@ function letsplay() {
 
   var questions = [
     {
-    question: chalk.bold.cyan(("Who's my favourite hero?") + chalk.bold.blue("\nA.Spiderman \nB.Batman \nC.Shaang Chi \nD.StarLord\n")),
+    question: chalk.bold.cyan(("Who's the first avenger?") + chalk.bold.blue("\nA.Captain America \nB.Batman \nC.Shaang Chi \nD.StarLord\n")),
     answer: "A"
   },{
-    question: chalk.bold.cyan(("What's my pet name?") + chalk.bold.blue("\nA.Sree \nB.Chinnu \nC.Santu \nD.Raju\n")),
+    question: chalk.bold.cyan(("Who killed Tony Stark's parents?") + chalk.bold.blue("\nA.Black Widow \nB.Hydra \nC.Winter Soldier \nD.Obadiah Stane\n")),
     answer: "C"
   },
   {
-    question: "Which is my Favouite Color? \nA.Pitch Black\nB.Bloody Red\nC.Persian Blue\nD.Neon Green\n",
-    answer: "A"
+    question: "Which got stuck in Sakaar? \nA.Iron Man\nB.Hawkeye\nC.Hulk\nD.Ultron\n",
+    answer: "C"
   },
   {
-    question: "Which is my favourite Cinematic Universe?\nA.Marvel \nB.DC \nC.The Boys \nD.Star Wars\n",
-    answer: "A"
+    question: "Which is the killer of avengers in What If?\nA.Loki \nB.Winter Soldier \nC.Antman \nD.Heimdall\n",
+    answer: "C"
   },
   {
-    question: "Which language am I most comfortable in? \nA.Malayalam \nB.Hindi \nC.Kannada \nD.English\n",
+    question: "Which language does thor study as an elective? \nA.Kree \nB.Groot \nC.Nelvayu \nD.Shiväisith\n",
     answer: "B"
   }]
 
